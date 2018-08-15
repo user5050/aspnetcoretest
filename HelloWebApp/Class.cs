5c0f7f009466a30c6f32154a0c6bbfa2c25b0f72
hy_006 <hy_006@hotmial.com>
2018-08-15 10:19:30 +0800

    add test

---
+++ HelloWebApp/Class.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWebApp
+{
+    [TestClass]
+    public class UTestClass
+    {
+        [testc]
+        public void Test()
+        {
+
+        }
+    }
+}
